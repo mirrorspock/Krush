eef14a3bcf8978e906c21afb2e96b880875ddc48
Kai-Arne Reiter <kai@mirrorspock.com>
2014-01-07 21:45:27 +0100

    placed tower storyboard

--- Krush/KAIGame.h
+++ Krush/KAIGame.h
@@ -12,6 +12,13 @@
 
 int TankX;
 int TankY;
+int CreditNumber;
+
+BOOL Tower1;
+BOOL Tower2;
+BOOL Tower3;
+BOOL Tower4;
+BOOL Tower5;
 
 
 @interface KAIGame : UIViewController
@@ -21,14 +28,49 @@ int TankY;
     IBOutlet UIImageView *Corner1;
     IBOutlet UIImageView *Corner2;
     
-    IBOutlet UIButton * NextWave;
+    IBOutlet UIButton *NextWave;
+    
+    IBOutlet UIButton *Tower1Space;
+    IBOutlet UIButton *Tower2Space;
+    IBOutlet UIButton *Tower3Space;
+    IBOutlet UIButton *Tower4Space;
+    IBOutlet UIButton *Tower5Space;
+
+    IBOutlet UIImageView *Tower1Dangerzone;
+    IBOutlet UIImageView *Tower2Dangerzone;
+    IBOutlet UIImageView *Tower3Dangerzone;
+    IBOutlet UIImageView *Tower4Dangerzone;
+    IBOutlet UIImageView *Tower5Dangerzone;
+    
+    IBOutlet UIImageView *Tower1Bullet;
+    IBOutlet UIImageView *Tower2Bullet;
+    IBOutlet UIImageView *Tower3Bullet;
+    IBOutlet UIImageView *Tower4Bullet;
+    IBOutlet UIImageView *Tower5Bullet;
+    
+    IBOutlet UIButton *NewTower;
+    IBOutlet UIButton *CancelNewTower;
+    
+    IBOutlet UILabel *Credits;
     
     NSTimer *Movement;
     
 }
 
+
 -(IBAction)NextWave:(id)sender;
 -(void)Moving;
+-(IBAction)NewTower:(id)sender;
+-(IBAction)CancelNewTower:(id)sender;
+
+-(IBAction)Tower1Space:(id)sender;
+-(IBAction)Tower2Space:(id)sender;
+-(IBAction)Tower3Space:(id)sender;
+-(IBAction)Tower4Space:(id)sender;
+-(IBAction)Tower5Space:(id)sender;
+
+-(void)TowerPlaced;
+
 
 
 
